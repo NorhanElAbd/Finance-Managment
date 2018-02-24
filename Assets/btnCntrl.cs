a3bf9fd5234730bc404ee83eb73674fb239eb786
NorhanElAbd <30782683+NorhanElAbd@users.noreply.github.com>
2018-02-24 22:19:21 +0200

    Initial commit

---
+++ Assets/btnCntrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnCntrl : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void showPanel(GameObject showPanel){
+		showPanel.SetActive (true);
+	}
+
+	public void hidePanel(GameObject hidePanel){
+		
+	}
+}
